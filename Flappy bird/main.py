421b2bdae9bcb4398f24f13b38bcc90e26a75c05
Anton <anton.vosc@gmail.com>
2020-08-25 14:51:48 +0100

    Flappy bird final

--- Flappy bird/main.py
+++ Flappy bird/main.py
@@ -1,4 +1,4 @@
-import pygame,random
+import pygame,random,time
 pygame.init()
 
 FPS = 100
@@ -13,58 +13,101 @@ black = (0, 0, 0)
 white = (255,255,255)
 
 class Ball(pygame.sprite.Sprite):
-	def __init__(self,color,size,name):
+	def __init__(self,color,size,name,x_speed):
 		pygame.sprite.Sprite.__init__(self)
 		self.image=pygame.Surface((size*2,size*2))
 		self.image.fill(white)
 		pygame.draw.circle(self.image,color,(size,size),size,0)
 		self.rect=self.image.get_rect()
 		self.size = self.rect.size
-		self.x_speed = 2
+		self.x_speed = x_speed
 		self.name = name
 		if self.name == 'player':
 			self.rect.x = (window_width/2) - (self.size[0]/2)
 			self.rect.y = (window_height/2) - (self.size[1]/2)
 		if self.name == 'enemy':
 			self.rect.x = (window_width) - (self.size[0])
-			self.rect.y = random.randint(0,window_height)
+			self.rect.y = random.randint(0,window_height-self.size[1])
 		self.y_speed = 1
+
 
 	def update(self):
 		if self.name == 'enemy':
 			self.rect.x -= self.x_speed
 
 
+
+def drawtext(text, x, y, color, size):
+    myfont = pygame.font.SysFont('Comic Sans MS', size)
+    textsurface = myfont.render(text,False, color)
+    gameDisplay.blit(textsurface,(x, y))
+
+
+def game_over(secs):
+	gameDisplay.fill(white)
+	drawtext('Game over.', (window_width/2)-150, (window_height/2)-150, green, 50)
+	drawtext('You have managed to survive for '+str(secs)+' seconds', (window_width/2)-350, (window_height/2)-50, green, 35)
+	pygame.display.flip()
+	time.sleep(10)
+	pygame.quit()
+
+
 def gameloop():
 	game_quit = False
+	obs_speed = 1
+	dir = -2
+	start_time = pygame.time.get_ticks()
+
 	player_gp = pygame.sprite.Group()
 	enemy_gp = pygame.sprite.Group()
-
-	ball = Ball(green,10,'player')
+	ball = Ball(green,10,'player',0)
 	player_gp.add(ball)
-
-	for i in range(5):
-		obsticle = Ball(black,12,'enemy')
-		enemy_gp.add(obsticle)
 
 	while game_quit!=True:
 		clock.tick(FPS)
-		gameDisplay.fill(white)
+		now = pygame.time.get_ticks()
+		seconds = (now-start_time) // 1000
 
 		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				pygame.quit()
+				quit()
 			if event.type==pygame.KEYDOWN:
 				if event.key==pygame.K_ESCAPE:
 					pygame.quit()
 					quit()
 				if event.key==pygame.K_SPACE:
-					ball.rect.y -= 10
-		ball.rect.y += 1
+					dir = 6
+			if event.type == pygame.KEYUP:
+				if event.key==pygame.K_SPACE:
+					dir = -2
 
+		ball.rect.y -= dir
+		obs_speed += 0.001
+		#crash check
+		crash = pygame.sprite.groupcollide(player_gp,enemy_gp,False,False)
+		if crash:
+			finish = pygame.time.get_ticks()
+			seconds = (finish-start_time) // 1000
+			game_over(seconds)
+		#top and bot check
+		if ball.rect.y < 0 or ball.rect.y > window_height:
+			finish = pygame.time.get_ticks()
+			seconds = (finish-start_time) // 1000
+			game_over(seconds)
+		#new spawn
+		prob = random.randint(0,100)
+		if prob <= 5:
+			obsticle = Ball(black,12,'enemy',obs_speed)
+			enemy_gp.add(obsticle)
+		# frame update
 		player_gp.update()
 		enemy_gp.update()
+		gameDisplay.fill(white)
+		pygame.draw.rect(gameDisplay, (255,0,0), (-6, 0, window_width+12, window_height) ,5)
 		player_gp.draw(gameDisplay)
 		enemy_gp.draw(gameDisplay)
-
+		drawtext('Time: '+str(seconds), (window_width/2)-500, 0, green, 35)
 		pygame.display.flip()
 
 gameloop()
