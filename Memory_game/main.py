6b86c15f12dcd735952353394343fc61e4d2c72e
Anton <anton.vosc@gmail.com>
2020-12-04 16:40:22 +0000

    final memory game

--- Memory_game/main.py
+++ Memory_game/main.py
@@ -6,17 +6,19 @@ clock = pygame.time.Clock()
 
 window_width=1000
 window_height=1000
-gamedisplay = pygame.display.set_mode((window_width,window_height))#pygame.FULLSCREEN #show display with the dimensions set
-pygame.display.set_caption('Memory game') #the name of the poject (appears at the top of the game window as a name of a game)
+gamedisplay = pygame.display.set_mode((window_width,window_height))
+pygame.display.set_caption('Memory game')
 
 images1 = ['images/cat1.png','images/cat2.png','images/cat3.png','images/cat4.png','images/cat1.png','images/cat2.png','images/cat3.png','images/cat4.png','images/cat5.png','images/cat6.png','images/cat7.png','images/cat8.png','images/cat5.png','images/cat6.png','images/cat7.png','images/cat8.png']
 images2 = ['images/cat1.png','images/cat2.png','images/cat1.png','images/cat2.png']
 images3 = ['images/cat1.png','images/cat2.png','images/cat3.png','images/cat4.png','images/cat1.png','images/cat2.png','images/cat3.png','images/cat4.png','images/cat5.png','images/cat6.png','images/cat7.png','images/cat8.png','images/cat5.png','images/cat6.png','images/cat7.png','images/cat8.png','images/cat9.png','images/cat10.png','images/cat11.png','images/cat12.png','images/cat9.png','images/cat10.png','images/cat11.png','images/cat12.png','images/cat13.png','images/cat14.png','images/cat15.png','images/cat16.png','images/cat13.png','images/cat14.png','images/cat15.png','images/cat16.png','images/cat17.png','images/cat18.png','images/cat17.png','images/cat18.png']
 
+
 def drawtext(text, x, y, color, size):
 	myfont = pygame.font.SysFont('Comic Sans MS', size)
 	textsurface = myfont.render(text,False, color)
 	gamedisplay.blit(textsurface,(x, y))
+
 
 def menu():
 	pygame.draw.rect(gamedisplay, (0,255,0), (window_width/8, (window_height/2)-75,150,150), 0)
@@ -27,23 +29,15 @@ def menu():
 	drawtext('6x6', (window_width/8)+640, (window_height/2)-25, (0,0,0), 35)
 
 
-class Game(pygame.sprite.Sprite):
+
+class Game():
 	def __init__(self):
-		global images1,images2
-		pygame.sprite.Sprite.__init__(self)
-		self.image=pygame.Surface((1,1))
-		self.image.fill((0,0,0))
-		self.rect=self.image.get_rect()
-		self.rect.x = 1
-		self.rect.y = 1
 		self.opened_cards = []
-		self.deletion_count = 0
 		self.first_click = False
 		self.start = 0
 		self.end = 0
 		self.last_update = 0
 		self.img_to_delete = None
-
 
 
 
@@ -68,7 +62,7 @@ class Cards(pygame.sprite.Sprite):
 		mouse_x, mouse_y = pygame.mouse.get_pos()
 		for events in event:
 			if events.type == pygame.MOUSEBUTTONDOWN:
-				if (mouse_x > self.rect.x) and (mouse_x < self.rect.x+self.size[0]) and (mouse_y > self.rect.y) and (mouse_y < self.rect.y+self.size[1]) and self.iamclicked==False:
+				if (mouse_x > self.rect.x) and (mouse_x < self.rect.x+self.size[0]) and (mouse_y > self.rect.y) and (mouse_y < self.rect.y+self.size[1]) and self.iamclicked==False and len(game.opened_cards)<2 and self.backturn==False:
 					game.last_update = pygame.time.get_ticks()
 					self.turn = True
 					self.iamclicked = True
@@ -119,62 +113,54 @@ class Cards(pygame.sprite.Sprite):
 					game.img_to_delete = None
 
 
-
-
-
-
 def drawtext(text, x, y, color, size):
 	myfont = pygame.font.SysFont('Comic Sans MS', size)
 	textsurface = myfont.render(text,False, color)
 	gamedisplay.blit(textsurface,(x, y))
 
 
-def clear_table():
-	for card in cards_gp:
-		card.kill()
-
-
 cards_gp = pygame.sprite.Group()
 game = Game()
-
 game_quit = False
+
+
 def gameloop():
 	timer = True
 	display_menu = True
+	opened_cards = []
 
 	but_size = 150
-
 	two_table_but_coords = (window_width/8,(window_height/2)-75,(window_width/8)+but_size,((window_height/2)-75)+but_size)
 	four_table_but_coords = ((window_width/8)+300, (window_height/2)-75, ((window_width/8)+300)+but_size, ((window_height/2)-75)+but_size)
 	six_table_but_coords = ((window_width/8)+600, (window_height/2)-75, ((window_width/8)+600)+but_size,((window_height/2)-75)+but_size)
-
 	two = False
 	four = False
 	six = False
 
-	opened_cards = []
+	try:
+		with open('game_times.txt','r') as file:
+			lowest_times = file.read()
+			lowest_times = lowest_times.replace("[","")
+			lowest_times = lowest_times.replace("]","")
+			lowest_times = lowest_times.replace("'","")
+			lowest_times = lowest_times.split(",")
+			two_table_best_time = lowest_times[0]
+			four_table_best_time = lowest_times[1]
+			six_table_best_time = lowest_times[2]
+	except FileNotFoundError:
+		lowest_times = [10000,100000,100000]
+		with open('game_times.txt','w') as file:
+			file.write(str(lowest_times))
+		two_table_best_time = 100000
+		four_table_best_time = 100000
+		six_table_best_time = 100000
 
-	with open('game_times.txt','r') as file:
-		lowest_times = file.read()
-		print(lowest_times)
-		print(type(lowest_times))
-		lowest_times = lowest_times.replace("[","")
-		lowest_times = lowest_times.replace("]","")
-		lowest_times = lowest_times.replace("'","")
-#		lowest_times = lowest_times.replace("''","")
-		lowest_times = lowest_times.split(",")
-		print(lowest_times)
-		print(type(lowest_times))
-		two_table_best_time = lowest_times[0]
-		print(two_table_best_time)
-		four_table_best_time = lowest_times[1]
-		six_table_best_time = lowest_times[2]
-	#	print(lowest_times)
 
 	while game_quit!=True:
 		gamedisplay.fill((0,0,0))
 		mouse_x, mouse_y = pygame.mouse.get_pos()
 		clock.tick(FPS)
+
 		ev = pygame.event.get()
 		for event in ev:
 			if event.type == pygame.QUIT:
@@ -183,16 +169,14 @@ def gameloop():
 				if event.key==pygame.K_ESCAPE:
 					pygame.quit()
 					quit()
-				if event.key==pygame.K_a:
-					clear_table()
 
 		if display_menu == True:
 			menu()
 			mouse_x, mouse_y = pygame.mouse.get_pos()
 			if ((mouse_x > two_table_but_coords[0]) and (mouse_x < two_table_but_coords[2])) and ((mouse_y > two_table_but_coords[1]) and (mouse_y < two_table_but_coords[3])):
 				pres = pygame.mouse.get_pressed()
-				two = True
 				if pres[0]==1:
+					two = True
 					y=380
 
 					for i in range(2):
@@ -209,8 +193,8 @@ def gameloop():
 
 			if ((mouse_x > four_table_but_coords[0]) and (mouse_x < four_table_but_coords[2])) and ((mouse_y > four_table_but_coords[1]) and (mouse_y < four_table_but_coords[3])):
 				pres = pygame.mouse.get_pressed()
-				four = True
 				if pres[0]==1:
+					four = True
 					y=220
 
 					for i in range(4):
@@ -227,8 +211,8 @@ def gameloop():
 
 			if ((mouse_x > six_table_but_coords[0]) and (mouse_x < six_table_but_coords[2])) and ((mouse_y > six_table_but_coords[1]) and (mouse_y < six_table_but_coords[3])):
 				pres = pygame.mouse.get_pressed()
-				six = True
 				if pres[0]==1:
+					six = True
 					y=60
 
 					for i in range(6):
@@ -242,6 +226,7 @@ def gameloop():
 						y += 150
 
 					display_menu = False
+
 
 		if len(cards_gp) == 0 and display_menu == False:
 			if timer == True:
@@ -281,6 +266,7 @@ def gameloop():
 					best_time = six_table_best_time
 				six = False
 
+
 			drawtext('You have played for '+str((game.end-game.start)//1000)+' seconds',(window_width/2)-440,(window_height/2)-70,(0,255,0),60)
 			drawtext('The best time for this group of games is '+str(best_time)+' seconds',(window_width/2)-440,(window_height/2)+70,(0,255,0),30)
 			pygame.display.flip()
@@ -289,6 +275,8 @@ def gameloop():
 		cards_gp.update(ev)
 		cards_gp.draw(gamedisplay)
 		pygame.display.flip()
+
+
 
 gameloop()
 pygame.quit()
