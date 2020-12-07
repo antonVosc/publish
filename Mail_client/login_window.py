f338cdb615d6f8945854747fdda6d5fc2f9b2bd6
Anton <anton.vosc@gmail.com>
2020-12-07 13:08:25 +0000

    holiday project to be continued on mac

---
+++ Mail_client/login_window.py
@@ -0,0 +1,88 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import smtplib
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(313, 150)
+        MainWindow.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(280, 0, 31, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(50, 20, 41, 16))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setEnabled(True)
+        self.lineEdit.setGeometry(QtCore.QRect(90, 20, 171, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.lineEdit.setText("")
+        self.lineEdit.setClearButtonEnabled(False)
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setEnabled(True)
+        self.lineEdit_2.setGeometry(QtCore.QRect(90, 60, 171, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setInputMethodHints(QtCore.Qt.ImhSensitiveData)
+        self.lineEdit_2.setText("")
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setClearButtonEnabled(False)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(30, 60, 61, 16))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(20, 100, 271, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.pushButton_2.clicked.connect(ui.login)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "?"))
+        self.label.setText(_translate("MainWindow", "Login:"))
+        self.label_2.setText(_translate("MainWindow", "Password:"))
+        self.pushButton_2.setText(_translate("MainWindow", "Log In"))
+
+    def login(self):
+        s = smtplib.SMTP('smtp.gmail.com',587)
+        s.starttls()
+        try:
+            s.login(self.lineEdit.text(),self.lineEdit_2.text())
+        except smtplib.SMTPAuthenticationError:
+            print('Log in failed! Check your details or try again later.')
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
