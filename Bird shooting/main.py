49f688b8e0ac8fd2da486cded749be646cfbc4d5
Anton <anton.vosc@gmail.com>
2021-07-21 18:00:28 +0300

    bird shooting

--- Bird shooting/main.py
+++ Bird shooting/main.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame,random
 
 pygame.init()
@@ -5,12 +7,17 @@ FPS = 60
 
 clock = pygame.time.Clock()
 window_width=1200 #getting my screen width from line 3
-window_height=1000 #getting my screen height from line
+window_height=800 #getting my screen height from line
 gameDisplay = pygame.display.set_mode((window_width,window_height))#pygame.FULLSCREEN #show display with the dimensions set
 pygame.display.set_caption('Bird Shooting') #the name of the poject (appears at the top of the game window as a name of a game)
 
 bg = pygame.image.load('images/bg.png')
 bg = pygame.transform.scale(bg,(window_width,window_height))
+
+def score(text, x, y, color, size):
+    myfont = pygame.font.SysFont('Comic Sans MS', size)
+    textsurface = myfont.render(text, False, color)
+    gameDisplay.blit(textsurface,(x, y))
 
 class Clouds(pygame.sprite.Sprite):
     def __init__(self):
@@ -45,7 +52,7 @@ class Bird(pygame.sprite.Sprite):
         self.endpoint = random.randint(100,window_width)
         self.delta = 0.7
 
-    def update(self):
+    def update(self,ball_gp):
         self.new_time = pygame.time.get_ticks()
         self.futureX += self.delta
         self.rect.centerx = self.futureX
@@ -61,6 +68,10 @@ class Bird(pygame.sprite.Sprite):
             self.counter = 1
         if self.rect.centerx > self.endpoint:
             self.delta = -1 * self.delta
+        death = pygame.sprite.spritecollide(self,ball_gp,True)
+        if death:
+            cannon.score += 1
+            self.kill()
 
 class Cannon(pygame.sprite.Sprite):
     def __init__(self):
@@ -68,27 +79,58 @@ class Cannon(pygame.sprite.Sprite):
         self.image = pygame.image.load('images/cannon/cannon_00.png')
         self.size = self.image.get_rect().size
         self.rect = self.image.get_rect()
-        self.rect.x = 800
-        self.rect.y = 450
-        self.angle = 50
+        self.rect.x = window_width - self.size[0] + 70
+        self.rect.y = window_height - self.size[1] - 175
+        self.angle = 5
         self.changed_angle = False
         self.dire = 1
+        self.score = 0
 
     def update(self):
         if self.changed_angle == True:
-            self.angle += 5 * self.dire
-             ]self.changed_angle = False
+            if self.angle>=5 and self.angle<=25:
+                self.angle += 5 * self.dire
+                self.changed_angle = False
+            else:
+                if self.angle == 0:
+                    self.angle = 5
+                elif self.angle == 30:
+                    self.angle = 25
         else:
             self.image = pygame.image.load('images/cannon/cannon_00.png')
-        self.old_centr = self.rect.center
-        self.image = pygame.transform.rotate(self.image, self.angle)
+            self.old_centr = self.rect.center
+            self.image = pygame.transform.rotate(self.image, self.angle)
+            self.rect = self.image.get_rect()
+            self.rect.center = self.old_centr
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self,x,y,angle):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/ball.png')
+        self.size = self.image.get_rect().size
+        self.image = pygame.transform.scale(self.image, (int(self.size[0]*0.1), int(self.size[1]*0.1)))
+        self.size = self.image.get_rect().size
         self.rect = self.image.get_rect()
-        self.rect.center = self.old_centr
+        self.rect.x = x
+        self.rect.y = y
+        self.x_power = 0
+        self.y_power = 0
+        self.angle = angle
+
+    def update(self):
+        self.y_power -= 1
+        self.rect.y -= self.y_power
+        self.rect.x -= self.angle
+        if self.rect.y > window_width:
+            self.kill()
+        if self.angle < 0:
+            self.x_power = self.angle
 
 def gameloop():
     cloud_gp = pygame.sprite.Group()
     birds_gp = pygame.sprite.Group()
     cannon_gp = pygame.sprite.Group()
+    ball_gp = pygame.sprite.Group()
 
     cannon = Cannon()
     cannon_gp.add(cannon)
@@ -106,6 +148,8 @@ def gameloop():
             bird = Bird()
             birds_gp.add(bird)
 
+        score('Score: '+str(cannon.score),0,window_height-200,(0,0,0),20)
+
         for event in pygame.event.get():
             if event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_ESCAPE:
@@ -117,14 +161,25 @@ def gameloop():
                 if event.key==pygame.K_RIGHT:
                     cannon.changed_angle = True
                     cannon.dire = -1
+                if event.key==pygame.K_SPACE:
+                    start = pygame.time.get_ticks()
+
+            if event.type==pygame.KEYUP:
+                if event.key==pygame.K_SPACE:
+                    finish = pygame.time.get_ticks()
+                    ball = Ball(cannon.rect.x+(cannon.size[0]/2),cannon.rect.y+(cannon.size[1]/2),cannon.angle)
+                    ball_gp.add(ball)
+                    ball.y_power = (finish - start)//50
 
         cloud_gp.update()
-        birds_gp.update()
+        birds_gp.update(ball_gp)
         cannon_gp.update()
+        ball_gp.update()
 
         cloud_gp.draw(gameDisplay)
         birds_gp.draw(gameDisplay)
         cannon_gp.draw(gameDisplay)
+        ball_gp.draw(gameDisplay)
 
         pygame.display.flip()
 
